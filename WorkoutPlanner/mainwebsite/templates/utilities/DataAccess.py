37e0622559655d2cff56710791d117de219c7a01
NotFriedEggs <158860111+HugoX1@users.noreply.github.com>
2025-06-19 12:19:04 +1000

    argon 2

--- WorkoutPlanner/mainwebsite/templates/utilities/DataAccess.py
+++ WorkoutPlanner/mainwebsite/templates/utilities/DataAccess.py
@@ -1,24 +1,57 @@
-# import pyodbc
+import pyodbc
+from argon2 import PasswordHasher
 
-# connection_string = (
-#     r"Driver={ODBC Driver 17 for SQL Server};"
-#     r"Server=10.221.64.20\SQLEXPRESS;"
-#     r"Database=Blackpoint;"
-#     f"UID=;"
-#     f"PWD=;"
-#     r"Column Encryption Setting=Enabled;"
-# )
+ph = PasswordHasher()
 
-# conn = pyodbc.connect(connection_string)
-# cursor = conn.cursor()
+connection_string = (
+    "Driver=ODBC Driver 17 for SQL Server;"
+    "Server=4CD717890BFCP\SQLEXPRESS;"
+    "Database=QuotingSystem;"
+    "UID=SoftE;"
+    "PWD=SoftE;")
 
-# def createUser(username, email, password, name):
-#     cursor = conn.cursor()
-    
-#     insert_query = '''
-#             INSERT INTO users (username, email, password_hash, full_name)
-#             VALUES (?, ?, ?, ?)
-#         '''
-        
-#     cursor.execute(insert_query, (username, email, password_hash, full_name))
-#     conn.commit()
+def verify_user(username, password):
+    try:
+        connection = pyodbc.connect(connection_string)
+        cursor = connection.cursor()
+
+        cursor.execute("SELECT userID, password_hash FROM users WHERE username = ?", (username,))
+        result = cursor.fetchone()
+       
+        cursor.close()
+        connection.close()
+
+        if result:
+            try:
+
+
+                if result and ph.verify(result[1], password):
+                   
+                    return "correct"
+            except Exception as error:
+                print(f"Error! Code: {type(error).__name__}, Message: {error}")
+        else:
+            return "Username or password incorrect! :("
+           
+       
+    except Exception as error:
+        print(f"Error! Code: {type(error).__name__}, Message: {error}")
+
+def getUserID(username):
+    try:
+        connection = pyodbc.connect(connection_string)
+        cursor = connection.cursor()
+
+        cursor.execute("SELECT userID FROM users WHERE username = ?", (username,))
+        result = cursor.fetchone()
+       
+        cursor.close()
+        connection.close()
+
+        userID = result[0]
+
+     
+        return userID  
+       
+    except Exception as error:
+        print(f"Error! Code: {type(error).__name__}, Message: {error}")
