e24f5a5a70d7d97053a73c7776a4d1b5140ee4a5
Hugo Watts <hugo.watts1@education.nsw.gov.au>
2025-06-20 12:08:49 +1000

    stuff

--- WorkoutPlanner/mainwebsite/templates/utilities/DataAccess.py
+++ WorkoutPlanner/mainwebsite/templates/utilities/DataAccess.py
@@ -4,18 +4,19 @@ from argon2 import PasswordHasher
 ph = PasswordHasher()
 
 connection_string = (
-    "Driver=ODBC Driver 17 for SQL Server;"
-    "Server=4CD717890BFCP\SQLEXPRESS;"
-    "Database=QuotingSystem;"
-    "UID=SoftE;"
-    "PWD=SoftE;")
+    r"Driver=ODBC Driver 17 for SQL Server;"
+    r"Server=DESKTOP-T0MNT6G\SQLEXPRESS;"
+    r"Database=WorkoutPlanner;"
+    r"UID=TEST;"
+    r"PWD=TEST123;"
+    r"Column Encryption Setting=Enabled;")
 
 def verify_user(username, password):
     try:
         connection = pyodbc.connect(connection_string)
         cursor = connection.cursor()
 
-        cursor.execute("SELECT userID, password_hash FROM users WHERE username = ?", (username,))
+        cursor.execute("SELECT user_id, password_hash FROM users WHERE username = ?", (username,))
         result = cursor.fetchone()
        
         cursor.close()
@@ -23,19 +24,39 @@ def verify_user(username, password):
 
         if result:
             try:
-
-
                 if result and ph.verify(result[1], password):
-                   
-                    return "correct"
+                    return True
             except Exception as error:
                 print(f"Error! Code: {type(error).__name__}, Message: {error}")
         else:
-            return "Username or password incorrect! :("
-           
-       
+            return False
+        
     except Exception as error:
         print(f"Error! Code: {type(error).__name__}, Message: {error}")
+
+def sign_up(username, email, password):
+    print('signing up')
+    
+    try:
+        connection = pyodbc.connect(connection_string)
+        cursor = connection.cursor()
+
+        password_hash = ph.hash(password)
+
+        print(username, email, password)
+
+        insert_query = '''
+            INSERT INTO users (full_name, email, password_hash)
+            VALUES (?, ?, ?)
+        '''
+        
+        cursor.execute(insert_query, (username, email, password_hash))
+        connection.commit()
+        return True
+
+    except Exception as error:
+        print(f"Error! Code: {type(error).__name__}, Message: {error}")
+        
 
 def getUserID(username):
     try:
@@ -49,9 +70,24 @@ def getUserID(username):
         connection.close()
 
         userID = result[0]
-
-     
         return userID  
+    
+    except Exception as error:
+        print(f"Error! Code: {type(error).__name__}, Message: {error}")
+        
+def getDataFromID(userId):
+    try:
+        connection = pyodbc.connect(connection_string)
+        cursor = connection.cursor()
+
+        cursor.execute("SELECT full_name, email FROM users WHERE user_id = ?", (userId,))
+        result = cursor.fetchone()
        
+        cursor.close()
+        connection.close()
+
+        print(result)
+        return result  
+    
     except Exception as error:
         print(f"Error! Code: {type(error).__name__}, Message: {error}")
--- WorkoutPlanner/mainwebsite/views.py
+++ WorkoutPlanner/mainwebsite/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 import mainwebsite.templates.utilities.DataAccess as data
 import datetime
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+       
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+       
+        verify_details = data.verify_user(username, password)
+        userID = data.getUserID(username)
+
+        if verify_details == False:
+            return render(request, 'mainwebsite/index.html', {'verify_details': verify_details}) 
+        elif verify_details == True:
+           request.session['userID'] = userID
+           return redirect('dashboard')
+       
+    else:
+        return render(request, 'mainwebsite/index.html')
+
     return render(request, 'mainwebsite/index.html')
 
 def dashboard(request):
@@ -15,10 +32,16 @@ def dashboard(request):
 def signup(request):
     if request.method == 'POST':
         name = request.POST.get('name')
-        lastname = request.POST.get('lastname')
         email = request.POST.get('email')
         password = request.POST.get('password')
 
+        print(name, email, password)
 
+        verify_details = data.sign_up(name, email, password)
 
+        if verify_details == False:
+            return render(request, 'mainwebsite/signup.html', {'verify_details': verify_details})
+        else:
+            return redirect('mainwebsite/index.html')  
+        
     return render(request, 'mainwebsite/signup.html')
